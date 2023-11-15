733bc986f011a05008f1f267949cfc9596c44c3a
Karan Janthe <karanjanthe@gmail.com>
2023-11-15 18:38:41 +0530

    rustlings complted till ch4 of book going for vec

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty: i32) -> i32 {
+    if(qty > 40){
+        qty
+    } else {
+        qty * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
