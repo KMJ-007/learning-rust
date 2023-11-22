1831761846260e237be3bdca9048d04c89604f77
Karan Janthe <karanjanthe@gmail.com>
2023-11-22 10:09:36 +0530

    move_semantics done

--- rustlings/exercises/move_semantics/move_semantics5.rs
+++ rustlings/exercises/move_semantics/move_semantics5.rs
@@ -6,14 +6,15 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
+// so basically when y is initialized the borrow checker gives the mut reference of x to the y, but then z also comes and it can't give the reference because it already given to the y, so i move the y's operation before the z's initialization, compiler will do it's job
 
 #[test]
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
