65050dcbacac7806f5cbe4b6d8ac8e2f64f684ac
Karan Janthe <karanjanthe@gmail.com>
2023-11-22 13:48:24 +0530

    hashmap done

--- rustlings/exercises/hashmaps/hashmaps3.rs
+++ rustlings/exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -34,11 +34,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        if scores.contains_key(&team_1_name){
+            let previous_score = scores.get(&team_1_name).unwrap();
+            // println!("{:?}",previous_score);
+            scores.insert(team_1_name,Team{ goals_scored: previous_score.goals_scored + team_1_score, goals_conceded: previous_score.goals_conceded + team_2_score}); 
+        } else {
+            scores.insert(team_1_name,Team{ goals_scored: team_1_score, goals_conceded: team_2_score});
+        }
+
+        if scores.contains_key(&team_2_name){
+            let previous_score = scores.get(&team_2_name).unwrap();
+            scores.insert(team_2_name,Team{ goals_scored: previous_score.goals_scored + team_2_score, goals_conceded: previous_score.goals_conceded + team_1_score}); 
+        } else {
+            scores.insert(team_2_name,Team{ goals_scored: team_2_score, goals_conceded: team_1_score});
+        }
     }
     scores
 }
