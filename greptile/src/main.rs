4b13e0b5115767fe71f16490726d0610d11b7b34
karan <karanjanthe@gmail.com>
2024-10-28 03:23:11 +0530

    basic grep

---
+++ greptile/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let search_term = "books";
+
+    let quote = "\
+    Every face, every shop, bedroom window, public-house, and
+    dark square is a picture feverishly turned--in search of what?
+    It is the same with books.
+    What do we seek through millions of pages?";
+
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            println!("{}", line);
+        }
+    }
+}
